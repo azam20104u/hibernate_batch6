6958a475d7e5caf153593bb916f6b2785219d907
azam20104u <azam20104u@gmail.com>
2024-05-25 10:21:36 +0530

    One2One relationship

--- src/main/java/com/azamda/hibernate/One2One.java
+++ src/main/java/com/azamda/hibernate/One2One.java
@@ -18,7 +18,10 @@ public class One2One {
 
         // session.persist(country);
 
-        
+        CountryDTO countryDTO = session.get(CountryDTO.class, 1);
+        PMDTO pmdto = countryDTO.getPmdto();
+        System.out.println(countryDTO);
+        System.out.println(pmdto);
         beginTransaction.commit();
         System.out.println(" Country and PM has been persisted");
         session.close();
